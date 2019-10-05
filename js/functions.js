84e81ba036bbe7c842bb2cb70a9bffd9cf5f1c89
Ramunas <rambek@gmail.com>
2019-10-05 12:54:26 +0300

    Freelance balance test

--- js/functions.js
+++ js/functions.js
@@ -11,48 +11,23 @@ function renderTable(data) {
         if ( i+1 !== item.month ) continue;
         
         else {
-            HTML += `<div class="table-row">
-                        <div class="cell">${i+1}</div>
-                        <div class="cell">${item.month}</div>
-                        <div class="cell">${item.income}.00 Eur</div>
-                        <div class="cell">${item.expense}.00 Eur</div>
-                        <div class="cell">${item.income - item.expense}.00 Eur</div>
-                    </div>`;
-        }
+            for (let x = 1; x <data.length; x++){
+                if ( x === i+1)
+                
+                HTML += `<div class="table-row">
+                <div class="cell">${i+1}</div>
+                <div class="cell">${item.month}</div>
+                <div class="cell">${item.income}.00 Eur</div>
+                <div class="cell">${item.expense}.00 Eur</div>
+                <div class="cell">${item.income - item.expense}.00 Eur</div>
+                </div>`;
+                // x = x + 1;
+                
+                // else { continue;
+                   
+       }}
     }}
 
     return document.querySelector('.table-content').innerHTML = HTML;
 }
 
-
-
-// function renderSkills( data ) {
-//     let HTML = '';
-
-//     data.forEach( skill => {
-//         if ( !skill.title ||
-//              typeof(skill.title) !== 'string' ||
-//              skill.title === 0 ||
-//              skill.title > 50 ||
-//              !skill.value ||
-//              typeof(skill.value) !== 'number' ||
-//              skill.value > 100 ||
-//              skill.value < 0 ) {
-//             return;
-//         }
-//         
-//         HTML += `<div class="progress-bar">
-//                     <div class="texts">
-//                         <div class="title">${skill.title}</div>
-//                         <div class="value">${value}%</div>
-//                     </div>
-//                     <div class="bar">
-//                         <div class="value" style="width: ${value}%;">
-//                             <div class="loading"></div>
-//                         </div>
-//                     </div>
-//                 </div>`;
-//     });
-    
-//     return HTML;
-// }
