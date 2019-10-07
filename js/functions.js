fd1676c1c35b2298b5a4237bd343752433001d3c
Ramunas <rambek@gmail.com>
2019-10-07 17:58:21 +0300

    Months correct list

--- js/functions.js
+++ js/functions.js
@@ -7,27 +7,27 @@ function renderTable(data) {
         if (typeof item.income == 'undefined') item.income = 0;
         if (typeof item.expense == 'undefined') item.expense = 0;
 
-    for (let i = 0; i < data.length; i++) {
-        if ( i+1 !== item.month ) continue;
-        
-        else {
-            for (let x = 1; x <data.length; x++){
-                if ( x === i+1)
-                
+    // for (let i = 0; i < data.length; i++) {
+    //     if ( i+1 !== i+1 ) continue;
+    if (typeof(item.months) !== 'text') {
+        item.months = months;
+      } 
+    
+    
                 HTML += `<div class="table-row">
                 <div class="cell">${i+1}</div>
-                <div class="cell">${item.month}</div>
-                <div class="cell">${item.income}.00 Eur</div>
-                <div class="cell">${item.expense}.00 Eur</div>
-                <div class="cell">${item.income - item.expense}.00 Eur</div>
+                <div class="cell">${item.months[i]}</div>
+                <div class="cell">${item.income} Eur</div>
+                <div class="cell">${item.expense} Eur</div>
+                <div class="cell">${item.income - item.expense} Eur</div>
                 </div>`;
                 // x = x + 1;
                 
                 // else { continue;
                    
-       }}
-    }}
+       }
+    
 
     return document.querySelector('.table-content').innerHTML = HTML;
-}
+    }
 
